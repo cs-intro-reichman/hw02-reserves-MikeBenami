a285f9d9a9f8a09fe533ef78fb5a83d750c7fc70
MikeBenami <146843399+MikeBenami@users.noreply.github.com>
2024-01-30 15:36:48 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -3,7 +3,7 @@ public class Reverse {
         String str = args[0];
 	int i = str.length(str);	
         char m = str.charAt(i/2);
-        for( i > 0; i--){
+        for(int i; i > 0; i--){
            System.out.print(str.charAt(i));
         }
         System.out.println("The middle character is " + m);
