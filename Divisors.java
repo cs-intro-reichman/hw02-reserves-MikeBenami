231c5884c8b94edee220786ca5991285016f1d39
MikeBenami <146843399+MikeBenami@users.noreply.github.com>
2024-01-30 14:45:28 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -2,7 +2,7 @@
 public class Divisors {
 	public static void main (String[] args) {
              int d = Integer.parseInt(args[0]);
-             for( int i = 0; i <= d ; i++){
+             for( int i = 1; i <= d ; i++){
               if( d % i == 0){
                 System.out.println(i);
               }
