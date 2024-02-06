3e4acfc1b0f4e30265c63a1b4e06e8fefec83146
MikeBenami <mikebenamicylen@gmail.com>
2024-02-06 16:59:18 +0200

    update

--- InOrder.java
+++ InOrder.java
@@ -1,9 +1,14 @@
-/**
- *  Generates and prints random integers in the range [0,10),
- *  as long as they form a non-decreasing sequence.
- */
-public class InOrder {
-	public static void main (String[] args) {
-		//// Write your code here
+public class InOrder{
+	public static void main(String[] args) {
+		int a = (int)(Math.random()*10);
+		System.out.print( a + " ");
+		do {
+			int b = (int)(Math.random()*10);
+			if(b >= a){
+				System.out.print(b + " ");
+				a = b ;
+			} else{
+				break;}
+		}while(true);	
 	}
 }
--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,18 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int number = Integer.parseInt(args[0]);
+		int sum = 1;
+		String solution = number + "is a perfect number since " + number + " = 1"; 
+		for(int i = 2 ; i < number; i++){
+			if (number % i == 0) {
+				sum = sum + i;
+				solution = solution + " + " + i ;
+			}
+        }
+		if(number != sum ){
+			solution = number + " is not a perfect number";
+		} 
+        System.out.println(solution);
 	}
 }
