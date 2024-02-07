7c5f68ddc6c9265ea0388d4cdb3da0d0e0a0d60c
MikeBenami <mikebenamicylen@gmail.com>
2024-02-07 17:43:00 +0200

    Update OneOfEachStats1.java

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -1,11 +1,63 @@
-/**
- *  Computes some statistics about families in which the parents decide 
- *  to have children until they have at least one child of each gender.
- *  The program expects to get one command-line argument: an int value
- * 	that determines how many families to simulate.
- */
 public class OneOfEachStats1 {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
+    public static void main(String[] args) {
+        int T = Integer.parseInt(args[0]);
+        int totalChildren = 0;
+        int familiesWithTwoChildren = 0;
+        int familiesWithThreeChildren = 0;
+        int familiesWithFourOrMoreChildren = 0;
+        int maxCount = 0;
+        int mode = 2;
+
+        for (int i = 0; i < T; i++) {
+            int boyCount = 0;
+            int girlCount = 0;
+            int childCount = 0;
+
+            boolean haveBoy = false;
+            boolean haveGirl = false;
+
+            while (!haveBoy || !haveGirl) {
+                double randomNumber = Math.random();
+                if (randomNumber < 0.5) {
+                    boyCount++;
+                    haveBoy = true;
+                } else {
+                    girlCount++;
+                    haveGirl = true;
+                }
+                childCount++;
+            }
+
+            totalChildren += childCount;
+
+            if (childCount == 2) {
+                familiesWithTwoChildren++;
+            } else if (childCount == 3) {
+                familiesWithThreeChildren++;
+            } else {
+                familiesWithFourOrMoreChildren++;
+            }
+
+            if (familiesWithTwoChildren > maxCount) {
+                maxCount = familiesWithTwoChildren;
+                mode = 2;
+            }
+            if (familiesWithThreeChildren > maxCount) {
+                maxCount = familiesWithThreeChildren;
+                mode = 3;
+            }
+            if (familiesWithFourOrMoreChildren > maxCount) {
+                maxCount = familiesWithFourOrMoreChildren;
+                mode = 4;
+            }
+        }
+
+        double averageChildren = (double) totalChildren / T;
+
+        System.out.println("Average: " + averageChildren + " children to get at least one of each gender.");
+        System.out.println("Number of families with 2 children: " + familiesWithTwoChildren);
+        System.out.println("Number of families with 3 children: " + familiesWithThreeChildren);
+        System.out.println("Number of families with 4 or more children: " + familiesWithFourOrMoreChildren);
+        System.out.println("The most common number of children is " + mode + ".");
+    }
 }
